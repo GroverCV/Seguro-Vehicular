31d8e9251624d06e7b99064d35f2b90937e4a1c5
groverchv <groverchoquevillca80@gmail.com>
2024-10-14 17:26:17 -0400

    Register

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const styles = {
   wrapper: {
@@ -51,18 +51,50 @@ const styles = {
 
 const Register = () => {
   const [formData, setFormData] = useState({
-    nombre: '',
-    apellido: '',
-    email: '',
-    contrasena: '',
-    estado: 'true',
-    telefono: '',
-    celular: '',
-    direccion: '',
-    tipoUsuario_id: 1,
-    rol_id: 4,
+    nombre: "",
+    apellido: "",
+    email: "",
+    contrasena: "",
+    estado: "true",
+    ci: "",
+    celular: "",
+    direccion: "",
+    tipoUsuario_id: "",
+    rol_id: "",
+    pais_id: "",
+    ciudad_id: "",
   });
-  const [mensaje, setMensaje] = useState(''); // Estado para el mensaje
+
+  const [mensaje, setMensaje] = useState(""); // Estado para el mensaje
+  const [tiposUsuario, setTiposUsuario] = useState([]); // Lista de tipos de usuario
+  const [roles, setRoles] = useState([]); // Lista de roles
+  const [paises, setPaises] = useState([]); // Lista de países
+  const [ciudades, setCiudades] = useState([]); // Lista de ciudades
+
+  // Obtener tipos de usuario, roles, países y ciudades al cargar el componente
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [tipoUsuarioRes, rolRes, paisRes, ciudadRes] = await Promise.all([
+          axios.get(
+            "https://backend-seguros.campozanodevlab.com/api/tipo_usuario"
+          ),
+          axios.get("https://backend-seguros.campozanodevlab.com/api/roles"),
+          axios.get("https://backend-seguros.campozanodevlab.com/api/pais"),
+          axios.get("https://backend-seguros.campozanodevlab.com/api/ciudad"),
+        ]);
+
+        setTiposUsuario(tipoUsuarioRes.data);
+        setRoles(rolRes.data);
+        setPaises(paisRes.data);
+        setCiudades(ciudadRes.data);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -71,62 +103,68 @@ const Register = () => {
 
   const getUserIp = async () => {
     try {
-        const response = await fetch("https://api.ipify.org?format=json");
-        const data = await response.json();
-        return data.ip;
+      const response = await fetch("https://api.ipify.org?format=json");
+      const data = await response.json();
+      return data.ip;
     } catch (error) {
-        console.error("Error obteniendo IP:", error);
-        return "IP desconocida";
+      console.error("Error obteniendo IP:", error);
+      return "IP desconocida";
     }
-};
+  };
 
-const userId = localStorage.getItem("userId");
+  const userId = localStorage.getItem("userId");
 
-const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await axios.post('https://backend-seguros.campozanodevlab.com/api/usuarios', formData);
-        console.log('Usuario registrado:', response.data);
-        setMensaje('Usuario registrado correctamente.'); // Mensaje de éxito
+      const response = await axios.post(
+        "https://backend-seguros.campozanodevlab.com/api/usuarios",
+        formData
+      );
+      console.log("Usuario registrado:", response.data);
+      setMensaje("Usuario registrado correctamente."); // Mensaje de éxito
 
-        // Registrar la acción en la bitácora
-        const userIp = await getUserIp();
-        const logData = {
-            usuario_id: userId,
-            accion: "Registró",
-            detalles: `El Usuario ID: ${userId} registró al Usuario ID: ${response.data.id}`,
-            ip: userIp,
-        };
+      // Registrar la acción en la bitácora
+      const userIp = await getUserIp();
+      const logData = {
+        usuario_id: userId,
+        accion: "Registró",
+        detalles: `El Usuario ID: ${userId} registró al Usuario ID: ${response.data.id}`,
+        ip: userIp,
+      };
 
-        await logAction(logData);
+      await logAction(logData);
     } catch (error) {
-        console.error('Error al registrar usuario:', error);
-        setMensaje('Error al registrar usuario: ' + (error.response?.data?.message || 'Error desconocido.')); // Mensaje de error
+      console.error("Error al registrar usuario:", error);
+      setMensaje(
+        "Error al registrar usuario: " +
+          (error.response?.data?.message || "Error desconocido.")
+      ); // Mensaje de error
     }
-};
+  };
 
-const logAction = async (logData) => {
+  const logAction = async (logData) => {
     const token = "simulated-token"; // Reemplazar con el token real si es necesario
     try {
-        await fetch("https://backend-seguros.campozanodevlab.com/api/bitacora", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(logData),
-        });
+      await fetch("https://backend-seguros.campozanodevlab.com/api/bitacora", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(logData),
+      });
     } catch (error) {
-        console.error("Error al registrar la acción en la bitácora:", error);
+      console.error("Error al registrar la acción en la bitácora:", error);
     }
-};
-
+  };
 
   return (
     <div style={styles.wrapper}>
       <form onSubmit={handleSubmit} style={styles.form}>
         <h2 style={styles.h2}>Registro de Usuario</h2>
-        {mensaje && <p style={styles.errorMessage}>{mensaje}</p>} {/* Mostrar mensaje */}
+        {mensaje && <p style={styles.errorMessage}>{mensaje}</p>}{" "}
+        {/* Mostrar mensaje */}
         <input
           type="text"
           name="nombre"
@@ -163,30 +201,95 @@ const logAction = async (logData) => {
           required
           style={styles.input}
         />
+        {/* Nuevo campo para CI */}
         <input
           type="number"
-          name="telefono"
-          placeholder="Teléfono"
-          value={formData.telefono}
+          name="ci"
+          placeholder="Carnet de Identidad"
+          value={formData.ci}
           onChange={handleChange}
+          required
           style={styles.input}
         />
+        {/* Nuevo campo para celular */}
         <input
           type="number"
           name="celular"
           placeholder="Celular"
           value={formData.celular}
           onChange={handleChange}
+          required
           style={styles.input}
         />
         <input
           type="text"
           name="direccion"
-          placeholder="Dirección"
+          placeholder="Direccion"
           value={formData.direccion}
           onChange={handleChange}
+          required
           style={styles.input}
         />
+        {/* Select Tipo Usuario */}
+        <select
+          name="tipoUsuario_id"
+          value={formData.tipoUsuario_id}
+          onChange={handleChange}
+          required
+          style={styles.input}
+        >
+          <option value="">Seleccione Tipo de Usuario</option>
+          {tiposUsuario.map((tipo) => (
+            <option key={tipo.id} value={tipo.id}>
+              {tipo.nombre}
+            </option>
+          ))}
+        </select>
+        {/* Select Rol */}
+        <select
+          name="rol_id"
+          value={formData.rol_id}
+          onChange={handleChange}
+          required
+          style={styles.input}
+        >
+          <option value="">Seleccione Rol</option>
+          {roles.map((rol) => (
+            <option key={rol.id} value={rol.id}>
+              {rol.nombre}
+            </option>
+          ))}
+        </select>
+        {/* Select País */}
+        <select
+          name="pais_id"
+          value={formData.pais_id}
+          onChange={handleChange}
+          required
+          style={styles.input}
+        >
+          <option value="">Seleccione País</option>
+          {paises.map((pais) => (
+            <option key={pais.id} value={pais.id}>
+              {pais.nombre}
+            </option>
+          ))}
+        </select>
+        {/* Select Ciudad */}
+        <select
+          name="ciudad_id"
+          value={formData.ciudad_id}
+          onChange={handleChange}
+          required
+          style={styles.input}
+        >
+          <option value="">Seleccione Ciudad</option>
+          {ciudades.map((ciudad) => (
+            <option key={ciudad.id} value={ciudad.id}>
+              {ciudad.nombre}
+            </option>
+          ))}
+        </select>
         <button type="submit" style={styles.button}>
           Registrar
         </button>
